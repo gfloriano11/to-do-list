09f827b55636a181a4e92a41cfabcb2d6a62e427
gfloriano11 <gflorianodev@gmail.com>
2024-12-02 09:55:45 -0300

    feat: create the form

--- code/frontend/script/add_task.js
+++ code/frontend/script/add_task.js
@@ -1,1 +1,9 @@
-let formulario = document.querySelector('.form_task')
+let form = document.querySelector('.form_task')
+let addTaskButton = document.querySelector('#add_task')
+
+addTaskButton.addEventListener('click', addTask)
+
+function addTask(){
+    console.log('removi!')
+    form.classList.remove('hide')
+}
