d7142560721342f6493b906e302b890cb29025f0
gfloriano11 <gflorianodev@gmail.com>
2024-12-04 12:15:54 -0300

    feat: responsive button add task and style the form!

--- code/frontend/script/add_task.js
+++ code/frontend/script/add_task.js
@@ -1,6 +1,11 @@
 let form = document.querySelector('.form_task')
 let addTaskButton = document.querySelector('#add_task')
+let body = document.querySelector('body')
+let titleContainer = document.querySelector('.title_container')
+let principal = document.querySelector('.principal')
 
 addTaskButton.addEventListener('click', () => { // function add task
     form.classList.remove('hide')
+    titleContainer.classList.add('opacidade')
+    principal.classList.add('opacidade')
 })
