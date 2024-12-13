374a2aea8931aab1a1acadebf20e3a8ea176c833
gfloriano11 <gflorianodev@gmail.com>
2024-12-12 23:27:09 -0300

    fix: change some docs names to padronization

--- code/frontend/script/add_task.js
+++ code/frontend/script/add_task.js
@@ -1,20 +1,20 @@
-const form = document.querySelector('.form_task')
-const addTaskButton = document.querySelector('#add_task')
-const body = document.querySelector('body')
-const titleContainer = document.querySelector('.title_container')
-const principal = document.querySelector('.principal')
-const cancelButton = document.querySelector('.cancel_button')
+const form = document.querySelector('.form_task');
+const addTaskButton = document.querySelector('#add_task');
+const body = document.querySelector('body');
+const titleContainer = document.querySelector('.title_container');
+const principal = document.querySelector('.principal');
+const cancelButton = document.querySelector('.cancel_button');
 
 addTaskButton.addEventListener('click', () => { // function add task
-    form.classList.remove('hide')
-    form.classList.add('central')
-    titleContainer.classList.add('opacidade')
-    principal.classList.add('opacidade')
+    form.classList.remove('hide');
+    form.classList.add('central');
+    titleContainer.classList.add('opacidade');
+    principal.classList.add('opacidade');
 })
 
 cancelButton.addEventListener('click', () => {
-    form.classList.add('hide')
-    form.classList.remove('central')
-    titleContainer.classList.remove('opacidade')
-    principal.classList.remove('opacidade')
+    form.classList.add('hide');
+    form.classList.remove('central');
+    titleContainer.classList.remove('opacidade');
+    principal.classList.remove('opacidade');
 })
--- code/frontend/script/confirm_task.js
+++ code/frontend/script/confirm_task.js
@@ -1,13 +1,13 @@
-const checks = document.querySelectorAll('.check')
+const checks = document.querySelectorAll('.check');
 
 checks.forEach((check) => {
     check.addEventListener('click', () => {
-        check.classList.remove('check')
-        check.classList.add('clicked_check')
+        check.classList.remove('check');
+        check.classList.add('clicked_check');
     
         const folder = 2;
     
         setTimeout(() => {
-        }, 3000)
+        }, 3000);
     })
 })
